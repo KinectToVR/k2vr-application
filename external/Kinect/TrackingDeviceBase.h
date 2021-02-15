beaee110975c21ad03b48c128906b5ab22d64bfb
公彦赤屋先 <kimihikoakaya@gmail.com>
2021-02-15 17:22:44 +0900

    Added a notice to TrackingDeviceBase header

--- external/Kinect/TrackingDeviceBase.h
+++ external/Kinect/TrackingDeviceBase.h
@@ -15,6 +15,9 @@ public:
 
 	}
 
+	// These 3 functions are critical
+	// One should be called before launching k2vr
+	// Update and shutdown is called by the k2vr itself
 	virtual void initialize() {};
 	virtual void shutdown() {};
 	virtual void update() {};
@@ -22,12 +25,17 @@ public:
 	virtual HRESULT getStatusResult() { return E_NOTIMPL; }
 	virtual std::string statusResultString(HRESULT stat) { return "statusResultString behaviour not defined"; };
 	
-	/* Handler for Tracking Joints, num 25 */
+	/*
+	 * Handler for Tracking Joints, num 25
+	 * You must update these during device's update function call
+	 */
 	glm::vec4 jointPositions[25] = { glm::vec4() };
 	glm::quat jointOrientations[25] = { glm::quat() };
-	
 	JointTrackingState trackingStates[25] = { 0 };
+
+	// Device type, specified in K2API
 	TrackingDeviceType deviceType = K2_Unknown;
-	
+
+	// Helper bool for knowing if skeleton't tracked
 	bool isSkeletonTracked = false;
 };
