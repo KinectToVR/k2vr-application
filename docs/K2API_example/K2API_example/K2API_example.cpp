d263b3e46143d802b0dbb061db91d5746d840885
Akayasaki Kimihiko <kimihikoakaya@gmail.com>
2021-03-13 19:45:12 +0900

    Added new K2API example project

--- docs/K2API_example/K2API_example/K2API_example.cpp
+++ docs/K2API_example/K2API_example/K2API_example.cpp
@@ -13,11 +13,11 @@ int main()
 	
 	std::cout << "Initializing server...\n";
 	
-	if(k2_api::init_socket(7135) == 0)
+	if(ktvr::init_socket(7135) == 0)
 		std::cout << "Initializing server succeed!\n";
 	else
 		std::cout << "Initializing server failed! Please see driver log for details.\n";
-
+	
 	/*
 	 * Add new tracker with global id.
 	 * To update it later we'll need its id in K2Driver,
@@ -25,64 +25,80 @@ int main()
 	 */
 
 	// Create tracker object with 0 position and orientation, see wiki
-	const K2Objects::K2TrackerBase base_tracker;
+	ktvr::K2TrackerBase tracker;
+	tracker.data.serial = "/devices/QTrack/WaistTracker";
+	tracker.data.role = ktvr::Tracker_Waist;
 
 	/*
 	 * If succeed, we're expecting everything more than -1
+	 * Also, since isActive is default 0,
+	 * tracker won't be spawned until we set it to true.
 	 */
 	
 	std::cout << "Adding new tracker...\n";
-	const auto id = k2_api::add_tracker(base_tracker);
-
-	if (id > -1) {
+	auto response = ktvr::add_tracker(tracker);
+	
+	// Check for the tracker id and success
+	if (tracker.id > -1 && response.success) {
 		// Display newly added tracker's id
-		std::cout << "Tracker added, id: " << id << '\n';
-
+		std::cout << "Tracker added, id: " << tracker.id << '\n';
+		
 		/*
 		 * Until tracker is added, we can edit its parameters,
 		 * let's make it appear at one meter height
-		 * and let's give it left foot tracker role hint!
+		 * and let's give it left foot tracker role
 		 * (Same can happen at base tracker declaration, see wiki)
 		 */
 
+		// Change the tracker's position, let the orientation be
+		tracker.pose.position = glm::vec3(0.f, 1.f, 0.f);
+
 		// Class conversion if we don't want to use K2PosePacket
-		const auto result_pose = k2_api::update_tracker_pose(id,
-			K2Objects::K2TrackerPose(glm::quat(), 
-				glm::vec3(0.f, 1.f, 0.f))); // y = 1.f: 1 meter height
+		const auto result_pose = 
+			ktvr::update_tracker_pose(tracker); // y = 1.f: 1 meter height
+		
+		// Change the tracker's role
+		tracker.data.serial = "/devices/JTrack/LFootTracker"; // Your funny serial
+		tracker.data.role = ktvr::Tracker_LeftFoot; // As described, left foot
+
+		// Since it wasn't spawned yet, it should spawn now
+		tracker.data.isActive = true; // If we change this, tracker should spawn
 		
 		// Class conversion if we don't want to use K2DataPacket
-		const auto result_data = k2_api::update_tracker_data(id,
-			K2Objects::K2TrackerData("/devices/KinectToVR/YunYunTrack", // Your funny serial
-				Tracker_LeftFoot)); // As described, left foot
-
+		const auto result_data = 
+			ktvr::update_tracker_data(tracker); // As described, left foot
+		
 		/*
-		 * Now, we're going to check operations results
-		 * We're expecting return code same as the tracker's id
+		 * Actually, you can update both at once,
+		 * via calling ktvr::update_tracker(tracker).
+		 * But actually, we're going to check those separately.
+		 * You may check for success or either, if is the id same as the tracker's one.
 		 */
-
+		
 		// Check the operation result (pose)
-		std::cout << (result_pose == id ? "Tracker pose has been changed!\n" :
+		std::cout << (result_pose.success ? "Tracker pose has been changed!\n" :
 			"Tracker pose has not been changed! Please see driver log for details.\n");
 		
 		// Check the operation result (data)
-		std::cout << (result_data == id ? "Tracker data has been changed!\n" :
+		std::cout << (result_data.success ? "Tracker data has been changed!\n" :
 			"Tracker data has not been changed! Please see driver log for details.\n");
-
-		/*
-		 * When everything is set-up then we can finally add our tracker
-		 * (Don't forget to check the result!)
-		 */
-
-		// Connect tracker (here, one) and expect 'true'
-		const auto connect = k2_api::connect_tracker(id);
-		if(connect == true)
-			std::cout << "Tracker has been connected!\n";
-		else
-			std::cout << "Tracker has not been connected! Please see driver log for details.\n";
 	}
-	else 
+	else {
 		std::cout << "Tracker not added! Please see driver log for details.\n";
-	
+		
+		/*
+		* At the end, always remember to disconnect.
+		* Otherwise the current process will crash,
+		* although everything will be executed normally.
+		*/
+
+		if (ktvr::close_socket() == 0)
+			std::cout << "Exit succeed!\n";
+		else
+			std::cout << "Socket closing failed! Please see driver log for details.\n";
+		return 0;
+	}
+
 	/*
 	 * Now that we added our tracker, we can update its pose
 	 * (first activation automatically adds it to devices' list)
@@ -90,10 +106,13 @@ int main()
 	 */
 
 	// Let's construct new pose for our driver in function call itself, see wiki
-	const auto update_pose = k2_api::update_tracker_pose(id,
-		K2Objects::K2TrackerPose(
-			glm::quat(glm::vec3(0.f, glm::radians(90.f), 0.f)),
-			glm::vec3(1.f, 1.f, 0.f)));
+	// Change the tracker's position, let the orientation be
+	tracker.pose.orientation = glm::vec3(0.f, 90.f, 0.f);
+	tracker.pose.position = glm::vec3(1.f, 1.f, 0.f);
+
+	// Class conversion if we don't want to use K2PosePacket
+	const auto result_pose =
+		ktvr::update_tracker_pose(tracker);
 
 	/*
 	 * glm::quat was constructed from vec3, that means we're converting eulers to quaternion.
@@ -102,7 +121,7 @@ int main()
 	 */
 
 	// Now, we're going to check the result
-	std::cout << (update_pose == id ? "Tracker pose has been changed!\n" :
+	std::cout << (result_pose.success ? "Tracker pose has been changed!\n" :
 		"Tracker pose has not been changed! Please see driver log for details.\n");
 
 	/*
@@ -111,14 +130,18 @@ int main()
 	 * although everything will be executed normally.
 	 */
 
-	if (k2_api::close_socket() == 0)
+	if (ktvr::close_socket() == 0)
 		std::cout << "Exit succeed!\n";
 	else
 		std::cout << "Socket closing failed! Please see driver log for details.\n";
 	
 	/*
 	 * For the rest of commands please see wiki, here I've described the most used ones:
-	 * Creation, getting results and post-creation updating pose
+	 * Creation, getting results and post-creation updating pose.
+	 * Recently, there was a function added to download already added tracker by id.
+	 * (Although only if it was added via K2API, it's not the inputemulator,
+	 * it can't overwrite some random device's data!)
+	 * Also, if an error occurs, you may check for the result code and it's name inside the enum!
 	 */
 	
 	return 0;
