a57410b348ceaac2aa0e46d77ca0f3bdb66ea8cc
公彦赤屋先 <kimihikoakaya@gmail.com>
2021-08-17 22:11:54 +0900

    Updated driver for new logging

--- driver_KinectToVR/K2ServerProvider.cpp
+++ driver_KinectToVR/K2ServerProvider.cpp
@@ -104,25 +104,12 @@ void K2WatchdogDriver::Cleanup()
 
 extern "C" __declspec(dllexport) void* HmdDriverFactory(const char* pInterfaceName, int* pReturnCode)
 {
-	char path[MAX_PATH];
-	HMODULE hm = NULL;
-
-	/* Get dll's path (not exe which loaded it nor execution location) */
-	GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, (LPCWSTR)&HmdDriverFactory, &hm);
-	GetModuleFileNameA(hm, path, sizeof path);
-	
-	/* Log to logs/ directory */
-	std::string _path(path);
-	_path.insert(_path.find("driver_KinectToVR.dll"), "logs\\");
-
-	/* Create logs/ directory */
-	CreateDirectoryA(
-		_path.substr(0, _path.find("\\driver_KinectToVR.dll")).c_str(), NULL);
+	// ktvr::GetK2AppDataFileDir will create all directories by itself
 	
 	/* Initialize logging */
-	google::InitGoogleLogging(_path.c_str());
+	google::InitGoogleLogging(ktvr::GetK2AppDataLogFileDir("driver_KinectToVR.dll").c_str());
 	/* Log everything >=INFO to same file */
-	google::SetLogDestination(google::GLOG_INFO, _path.c_str());
+	google::SetLogDestination(google::GLOG_INFO, ktvr::GetK2AppDataLogFileDir("driver_KinectToVR.dll").c_str());
 	google::SetLogFilenameExtension(".log");
 	
 	FLAGS_logbufsecs = 0; //Set max timeout
