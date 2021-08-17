43716f51f1efb8e365c8d222c82b22ab2551c4a9
公彦赤屋先 <kimihikoakaya@gmail.com>
2021-08-17 22:10:54 +0900

    Logging now logs to APPDATA/KinectToVR/logs

--- KinectToVR/Logging.cpp
+++ KinectToVR/Logging.cpp
@@ -1,33 +1,19 @@
 ﻿#include <Logging.h>
+#include "getVariable.h"
 
 /* Configure logging and print first message */
 KINECTTOVR_LIB void initLogging()
 {
-	char path[MAX_PATH];
-	HMODULE hm = NULL;
-
-	/* Get dll's path (not exe which loaded it nor execution location) */
-	GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, (LPCWSTR)&initLogging, &hm);
-	GetModuleFileNameA(hm, path, sizeof path);
-
-	/* Log to logs/ directory */
-	std::string _path(path);
-	_path.insert(_path.rfind("KinectToVR"), "logs\\");
-
-	/* Create logs/ directory */
-	CreateDirectoryA(
-		_path.substr(0, _path.rfind("\\KinectToVR")).c_str(), NULL);
-
-	/* Remove .dll substring from log name */
-	_path.erase(_path.rfind(".dll"), std::string(".dll").length());
+	// ktvr::GetK2AppDataFileDir will create all directories by itself
 
 	/* Initialize logging */
-	google::InitGoogleLogging(_path.c_str());
+	google::InitGoogleLogging(ktvr::GetK2AppDataLogFileDir("KinectToVR_APP").c_str());
 	/* Log everything >=INFO to same file */
-	google::SetLogDestination(google::GLOG_INFO, _path.c_str());
-	
+	google::SetLogDestination(google::GLOG_INFO, ktvr::GetK2AppDataLogFileDir("KinectToVR_APP").c_str());
+	google::SetLogFilenameExtension(".log");
+
 	FLAGS_logbufsecs = 0; //Set max timeout
-	FLAGS_minloglevel = google::GLOG_INFO; // Log everything
+	FLAGS_minloglevel = google::GLOG_INFO;
 	
 	LOG(INFO) << "~~~KinectToVR new logging session begins here!~~~";
 }
